1be21122aadc51bdbc11bb89d7f8cddd644d1280
unknown <rossifakhri@gmail.com>
2023-12-31 08:26:56 +0700

    Fixed main bugs

--- main.py
+++ main.py
@@ -15,22 +15,6 @@ if __name__=="__main__":
         print("2. Lihat Histori Latihan")
         print("0. Keluar")
 
-<<<<<<< HEAD
-        user_option = input("Masukan opsi: ")
-        match user_option:
-            case "1": CRUD.run_routine( )
-            case "2": CRUD.print_all_routines()
-            case "3": CRUD.new_routine()
-            case "4": CRUD.edit_routine()
-            case "5": CRUD.delete_routine()
-            case "6": CRUD.history_page()
-            case "0": break
-            case _: print("Opsi tidak tersedia")
-        
-        # is_continue = input("Keluar? (y/n): ")
-        # if is_continue.lower() == "y":
-        #     break
-=======
         while True:
             user_option = input("Masukan opsi: ")
             match user_option:
@@ -45,4 +29,3 @@ if __name__=="__main__":
                     break
                 case _: print("Opsi tidak tersedia")
             
->>>>>>> test
